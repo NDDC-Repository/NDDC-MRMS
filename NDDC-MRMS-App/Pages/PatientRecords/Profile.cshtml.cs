e46b4e21f32d8abbb52f4bd273fbb5d97f4ea429
Dash Dogg <advantellocloud@gmail.com>
2023-10-18 10:41:13 +0100

    update

---
+++ NDDC-MRMS-App/Pages/PatientRecords/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_MRMS_App.Pages.PatientRecords
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
