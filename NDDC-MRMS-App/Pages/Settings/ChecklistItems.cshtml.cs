0d492fbba25d7b5f8fe6dbf980ef363f04ff7fa4
Dash Dogg <advantellocloud@gmail.com>
2023-11-08 12:46:48 +0100

    update

---
+++ NDDC-MRMS-App/Pages/Settings/ChecklistItems.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_MRMS_App.Pages.Settings
+{
+    public class ChecklistItemsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
